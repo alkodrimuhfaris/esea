7098ded08aa84a5a540f759a52ecc14317784bc1
M Faris Alkodri <farisalkodri@gmail.com>
2021-01-24 10:24:43 +0700

    edit logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware} from 'redux';
-import {logger} from 'redux-logger';
+import logger from 'redux-logger';
 import promiseMiddleware from 'redux-promise-middleware';
 
 import rootReducer from './reducers';
